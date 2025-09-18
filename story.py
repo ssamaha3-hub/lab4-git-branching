cc6f4a03e279ad79b5064708f8e864093fed7bf3
Samer Samaha <233132729+ssamaha3-hub@users.noreply.github.com>
2025-09-18 18:25:21 +0000

    added villian path

--- story.py
+++ story.py
@@ -13,10 +13,12 @@ def intro():
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
     print("Then a massive dragon attacks you and you slay it in one hit.")
-    print("You head back to your village knowing you saved the day.")
+    print("You headed back to your village knowing you saved the day.")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
+    print("You slay the squirrel and take his acorns")
+    print("You take the acorns back to the village and show off your loot.")
 
 def center_path():
     print("you walk center and sit at a nearby lake.")
